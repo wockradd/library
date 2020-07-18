e5f7234332f29214d768639ab42d3dbff01b55f0
Lee Donovan <lee.donovan2303@gmail.com>
2020-07-18 21:45:25 +0100

    Updates

---
+++ precache-manifest.3020b531b4309848d6a2599b11c8b292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3440c263353322f7cc452fda220d40e",
+    "url": "/library/index.html"
+  },
+  {
+    "revision": "4cdf7145ff11bc6c5ed8",
+    "url": "/library/static/css/main.4733e3de.chunk.css"
+  },
+  {
+    "revision": "2ce22bf69ca2369071da",
+    "url": "/library/static/js/2.247f53e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/library/static/js/2.247f53e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdf7145ff11bc6c5ed8",
+    "url": "/library/static/js/main.1bf2d715.chunk.js"
+  },
+  {
+    "revision": "3e7644284c969e58940d",
+    "url": "/library/static/js/runtime-main.5d4a03c2.js"
+  }
+]);
